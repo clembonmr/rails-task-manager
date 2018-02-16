de1b7f7a42c6ee38e359f24a8e422ee3addfaca4
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-16 12:57:01 +0800

    completed list task feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ
+  # see lists of tasks
+  get 'tasks', to: 'tasks#index'
+  # see a specific task
+  get 'tasks/:id', to: 'tasks#show'
+
+
+
+
+  # CREATE
+  # get the form
+  get 'tasks/new', to: 'tasks#new'
+  # submit the form
+  post 'tasks', to: 'tasks#create'
+
+
+
+  # UPDATE
+  # get the form
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  #submit the form
+  patch 'tasks/:id', to: 'tasks#update'
+
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Deleting all tasks"
+Task.destroy_all
+puts "Creating 10 tasks"
+36.times do
+  tasks = Task.new(title: Faker::Hipster.sentence, details: Faker::ChuckNorris.fact, completed: [true, false].sample)
+  if tasks.save
+    puts tasks.title + " created!"
+  end
+end
+
+puts "#{Task.count} tasks created!"
